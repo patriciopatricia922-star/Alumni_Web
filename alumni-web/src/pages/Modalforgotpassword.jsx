5d3c9fdf6021bd9d97762e4fcf906e7150809873
PatPat <patriciopatricia922@gmail.com>
2026-08-05 09:34:51 +0800

    fix: standardize casing of ModalForgotPasswordView import in Modalforgotpassword.jsx

--- alumni-web/src/pages/Modalforgotpassword.jsx
+++ alumni-web/src/pages/Modalforgotpassword.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
-import ModalForgotPasswordView from '../Views/ModalForgotPasswordView';
+import ModalForgotPasswordView from '../Views/Modalforgotpasswordview';
 import ModalVerification from './ModalVerification';
 
 /**
